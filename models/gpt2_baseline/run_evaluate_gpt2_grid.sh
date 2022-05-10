30b3e579c7caf13fbe4b37d14f2cc38660e1edf5
Alejandro Santorum <alejandro.santorum@gmail.com>
2022-05-10 18:04:40 +0100

    Fix small bug on gpt evaluation script on grid

--- models/gpt2_baseline/run_evaluate_gpt2_grid.sh
+++ models/gpt2_baseline/run_evaluate_gpt2_grid.sh
@@ -14,5 +14,5 @@ qsub -l h=!stg-gpu5\&!stg-gpu6\&!stg-gpu7\&!stg-gpu8\&!stg-gpu9,virtual_gpu_free
     -o /rmt/dialogue2/interns/alejandro/logs \
     ${SCRIPTS_PATH}run_on_grid.sh gpt2_dst/scripts/evaluate.py \
         --input_path_target="${PATH_DIR}"/gpt2_dst/data/simmc2_dials_dstc10_devtest_target.txt \
-        --input_path_predicted="${PATH_DIR}"/gpt2_dst/data/simmc2_dials_dstc10_devtest_predicted.txt \
+        --input_path_predicted="${PATH_DIR}"/gpt2_dst/results/simmc2_dials_dstc10_devtest_predicted.txt \
         --output_path_report="${PATH_DIR}"/gpt2_dst/results/simmc2_dials_dstc10_devtest_report.json
