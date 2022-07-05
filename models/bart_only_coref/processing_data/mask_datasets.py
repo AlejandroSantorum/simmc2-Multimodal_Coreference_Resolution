5540da350f7e6597c772cb47f5331152516d251b
Alejandro Santorum <alejandro.santorum@gmail.com>
2022-07-05 16:38:22 +0100

    Add script to generate masked datasets

--- models/bart_only_coref/processing_data/mask_datasets.py
+++ models/bart_only_coref/processing_data/mask_datasets.py
@@ -13,6 +13,9 @@ DEV_TARGET_PROCESSED_PATH = "../data_object_special/simmc2_dials_dstc10_dev_targ
 DEVTEST_PRED_PROCESSED_PATH = "../data_object_special/simmc2_dials_dstc10_devtest_predict.txt"
 DEVTEST_TARGET_PROCESSED_PATH = "../data_object_special/simmc2_dials_dstc10_devtest_target.txt"
 
+DEVTEST_PRED_ONLY_FURN_PATH = "../data_object_special/new_datasets/devtest_only_furniture_predict.txt"
+DEVTEST_TARGET_ONLY_FURN_PATH = "../data_object_special/new_datasets/devtest_only_furniture_target.txt"
+
 
 def read_dataset(datapath):
     with open(datapath, 'r') as f:
@@ -41,12 +44,15 @@ def mask_dialogue(dataset, dataset_type='predict'):
     for line in dataset:
         soo_idx = line.find(keyword)
         dial_part = line[:soo_idx].strip()
+        #print(dial_part)
         dial_split = dial_part.split(' ')
         for i,word in enumerate(dial_split):
-            if (word not in ["User", "System", ':']) and ('<' not in word):
-                dial_split[i] = 'MASK'
+            if (word not in ["User", "System", ':', '']) and ('<' not in word):
+                dial_split[i] = ' '
 
-        masked_line = ' '.join(dial_split) + line[soo_idx:]
+        #print(' '.join(dial_split))
+        #exit()
+        masked_line = (' '.join(dial_split)).strip() + line[soo_idx:]
         masked_dataset.append(masked_line)
 
     return masked_dataset
@@ -88,6 +94,40 @@ def mask_global_ids(dataset):
     return masked_dataset
 
 
+def mask_bounding_boxes(dataset):
+    masked_dataset = []
+
+    for line in dataset:
+        start_idx = line.find('[(')
+        while start_idx != -1:
+            end_idx = line.find(')]', start_idx)
+            line = line[:start_idx] + '[(0.0,0.0,0.0,0.0,0.0,0.0' + line[end_idx:]
+            start_idx = line.find('[(', start_idx+1)
+
+        masked_dataset.append(line)
+    return masked_dataset
+
+
+def mask_canonical_ids(dataset):
+    masked_dataset = []
+
+    for line in dataset:
+        start_idx = line.find('<OBJ>')
+        while start_idx != -1:
+            end_idx = line.find('[(', start_idx)
+            line = line[:start_idx] + '<OBJ><199>' + line[end_idx:]
+            start_idx = line.find('<OBJ>', start_idx+1)
+        
+        start_idx = line.find('<PREVIOBJ>')
+        while start_idx != -1:
+            end_idx = line.find('[(', start_idx)
+            line = line[:start_idx] + '<PREVIOBJ><199>' + line[end_idx:]
+            start_idx = line.find('<PREVIOBJ>', start_idx+1)
+
+        masked_dataset.append(line)
+    return masked_dataset 
+
+
 def main():
     # train_pred = read_dataset(TRAIN_PRED_PROCESSED_PATH)
     # train_target= read_dataset(TRAIN_TARGET_PROCESSED_PATH)
@@ -95,13 +135,20 @@ def main():
     # dev_target= read_dataset(DEV_TARGET_PROCESSED_PATH)
     devtest_pred = read_dataset(DEVTEST_PRED_PROCESSED_PATH)
     devtest_target= read_dataset(DEVTEST_TARGET_PROCESSED_PATH)
+    devtest_only_furn_pred = read_dataset(DEVTEST_PRED_ONLY_FURN_PATH)
+    devtest_only_furn_target = read_dataset(DEVTEST_TARGET_ONLY_FURN_PATH)
 
+    # Standard devset
     DEVTEST_PRED_MASK_DIAL_STORE_PATH = STORE_BASE_PATH + "devtest_masked_dials_predict.txt"
     DEVTEST_TARGET_MASK_DIAL_STORE_PATH = STORE_BASE_PATH + "devtest_masked_dials_target.txt"
     DEVTEST_PRED_MASK_OBJMEN_STORE_PATH = STORE_BASE_PATH + "devtest_masked_objmen_predict.txt"
     DEVTEST_TARGET_MASK_OBJMEN_STORE_PATH = STORE_BASE_PATH + "devtest_masked_objmen_target.txt"
     DEVTEST_PRED_MASK_GLOBALIDS_STORE_PATH = STORE_BASE_PATH + "devtest_masked_globalIDs_predict.txt"
     DEVTEST_TARGET_MASK_GLOBALIDS_STORE_PATH = STORE_BASE_PATH + "devtest_masked_globalIDs_target.txt"
+    DEVTEST_PRED_MASK_BOXES_STORE_PATH = STORE_BASE_PATH + "devtest_masked_bboxes_predict.txt"
+    DEVTEST_TARGET_MASK_BOXES_STORE_PATH = STORE_BASE_PATH + "devtest_masked_bboxes_target.txt"
+    DEVTEST_PRED_MASK_CANONICAL_STORE_PATH = STORE_BASE_PATH + "devtest_masked_canonicalIDs_predict.txt"
+    DEVTEST_TARGET_MASK_CANONICAL_STORE_PATH = STORE_BASE_PATH + "devtest_masked_canonicalIDs_target.txt"
 
     print("Masking dialogues of devtest sets...")
     devtest_pred_masked_dial = mask_dialogue(devtest_pred, dataset_type='predict')
@@ -110,16 +157,70 @@ def main():
     _store_data(devtest_target_masked_dial, DEVTEST_TARGET_MASK_DIAL_STORE_PATH)
 
     print("Masking prev. mentioned objects of devtest sets...")
-    devtest_pred_masked_objmen = mask_mentioned_objs(devtest_pred,)
+    devtest_pred_masked_objmen = mask_mentioned_objs(devtest_pred)
     devtest_target_masked_objmen = mask_mentioned_objs(devtest_target)
     _store_data(devtest_pred_masked_objmen, DEVTEST_PRED_MASK_OBJMEN_STORE_PATH)
     _store_data(devtest_target_masked_objmen, DEVTEST_TARGET_MASK_OBJMEN_STORE_PATH)
 
     print("Masking global IDs of devtest sets...")
-    devtest_pred_masked_globalids = mask_global_ids(devtest_pred,)
+    devtest_pred_masked_globalids = mask_global_ids(devtest_pred)
     devtest_target_masked_globalids = mask_global_ids(devtest_target)
     _store_data(devtest_pred_masked_globalids, DEVTEST_PRED_MASK_GLOBALIDS_STORE_PATH)
     _store_data(devtest_target_masked_globalids, DEVTEST_TARGET_MASK_GLOBALIDS_STORE_PATH)
+
+    print("Masking bounding boxes of devtest sets...")
+    devtest_pred_masked_boxes = mask_bounding_boxes(devtest_pred)
+    devtest_target_masked_boxes = mask_bounding_boxes(devtest_target)
+    _store_data(devtest_pred_masked_boxes, DEVTEST_PRED_MASK_BOXES_STORE_PATH)
+    _store_data(devtest_target_masked_boxes, DEVTEST_TARGET_MASK_BOXES_STORE_PATH)
+
+    print("Masking canonical IDs of devtest sets...")
+    devtest_pred_masked_canonicalIDs = mask_canonical_ids(devtest_pred)
+    devtest_target_masked_canonicalIDs = mask_canonical_ids(devtest_target)
+    _store_data(devtest_pred_masked_canonicalIDs, DEVTEST_PRED_MASK_CANONICAL_STORE_PATH)
+    _store_data(devtest_target_masked_canonicalIDs, DEVTEST_TARGET_MASK_CANONICAL_STORE_PATH)
+
+    # Devtest only furniture
+    DEVTEST_FURN_PRED_MASK_DIAL_STORE_PATH = STORE_BASE_PATH + "devtest_only_furn_masked_dials_predict.txt"
+    DEVTEST_FURN_TARGET_MASK_DIAL_STORE_PATH = STORE_BASE_PATH + "devtest_only_furn_masked_dials_target.txt"
+    DEVTEST_FURN_PRED_MASK_OBJMEN_STORE_PATH = STORE_BASE_PATH + "devtest_only_furn_masked_objmen_predict.txt"
+    DEVTEST_FURN_TARGET_MASK_OBJMEN_STORE_PATH = STORE_BASE_PATH + "devtest_only_furn_masked_objmen_target.txt"
+    DEVTEST_FURN_PRED_MASK_GLOBALIDS_STORE_PATH = STORE_BASE_PATH + "devtest_only_furn_masked_globalIDs_predict.txt"
+    DEVTEST_FURN_TARGET_MASK_GLOBALIDS_STORE_PATH = STORE_BASE_PATH + "devtest_only_furn_masked_globalIDs_target.txt"
+    DEVTEST_FURN_PRED_MASK_BOXES_STORE_PATH = STORE_BASE_PATH + "devtest_only_furn_masked_bboxes_predict.txt"
+    DEVTEST_FURN_TARGET_MASK_BOXES_STORE_PATH = STORE_BASE_PATH + "devtest_only_furn_masked_bboxes_target.txt"
+    DEVTEST_FURN_PRED_MASK_CANONICAL_STORE_PATH = STORE_BASE_PATH + "devtest_only_furn_masked_canonicalIDs_predict.txt"
+    DEVTEST_FURN_TARGET_MASK_CANONICAL_STORE_PATH = STORE_BASE_PATH + "devtest_only_furn_masked_canonicalIDs_target.txt"
+
+    print("Masking dialogues of furniture of devtest sets...")
+    devtest_only_furnpred_masked_dial = mask_dialogue(devtest_only_furn_pred, dataset_type='predict')
+    devtest_only_furntarget_masked_dial = mask_dialogue(devtest_only_furn_target, dataset_type='target')
+    _store_data(devtest_only_furnpred_masked_dial, DEVTEST_FURN_PRED_MASK_DIAL_STORE_PATH)
+    _store_data(devtest_only_furntarget_masked_dial, DEVTEST_FURN_TARGET_MASK_DIAL_STORE_PATH)
+
+    print("Masking prev. mentioned objects of furniture of devtest sets...")
+    devtest_only_furnpred_masked_objmen = mask_mentioned_objs(devtest_only_furn_pred)
+    devtest_only_furntarget_masked_objmen = mask_mentioned_objs(devtest_only_furn_target)
+    _store_data(devtest_only_furnpred_masked_objmen, DEVTEST_FURN_PRED_MASK_OBJMEN_STORE_PATH)
+    _store_data(devtest_only_furntarget_masked_objmen, DEVTEST_FURN_TARGET_MASK_OBJMEN_STORE_PATH)
+
+    print("Masking global IDs of furniture of devtest sets...")
+    devtest_only_furnpred_masked_globalids = mask_global_ids(devtest_only_furn_pred)
+    devtest_only_furntarget_masked_globalids = mask_global_ids(devtest_only_furn_target)
+    _store_data(devtest_only_furnpred_masked_globalids, DEVTEST_FURN_PRED_MASK_GLOBALIDS_STORE_PATH)
+    _store_data(devtest_only_furntarget_masked_globalids, DEVTEST_FURN_TARGET_MASK_GLOBALIDS_STORE_PATH)
+
+    print("Masking bounding boxes of furniture of devtest sets...")
+    devtest_only_furnpred_masked_boxes = mask_bounding_boxes(devtest_only_furn_pred)
+    devtest_only_furntarget_masked_boxes = mask_bounding_boxes(devtest_only_furn_target)
+    _store_data(devtest_only_furnpred_masked_boxes, DEVTEST_FURN_PRED_MASK_BOXES_STORE_PATH)
+    _store_data(devtest_only_furntarget_masked_boxes, DEVTEST_FURN_TARGET_MASK_BOXES_STORE_PATH)
+
+    print("Masking canonical IDs of furniture of devtest sets...")
+    devtest_only_furnpred_masked_canonical = mask_canonical_ids(devtest_only_furn_pred)
+    devtest_only_furntarget_masked_canonical = mask_canonical_ids(devtest_only_furn_target)
+    _store_data(devtest_only_furnpred_masked_canonical, DEVTEST_FURN_PRED_MASK_CANONICAL_STORE_PATH)
+    _store_data(devtest_only_furntarget_masked_canonical, DEVTEST_FURN_TARGET_MASK_CANONICAL_STORE_PATH)
 
     return
 
