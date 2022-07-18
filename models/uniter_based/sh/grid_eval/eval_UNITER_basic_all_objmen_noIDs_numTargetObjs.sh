aacb3c1c24d844f4b4884d67f8bd7431e4eadf18
Alejandro Santorum <alejandro.santorum@gmail.com>
2022-07-18 11:54:47 +0100

    Add UNITER scripts with new N target head

---
+++ models/uniter_based/sh/grid_eval/eval_UNITER_basic_all_objmen_noIDs_numTargetObjs.sh
@@ -0,0 +1,23 @@
+SCRIPTS_PATH="/rmt/dialogue2/interns/alejandro/scripts/"
+
+cd ../../
+
+qsub -l h=stg-gpu25,virtual_gpu_free=8000M,gpu=1,gpu_queue=1,arch=*64*,test=*,centos=1,gpu_installed=1 \
+    -e /rmt/dialogue2/interns/alejandro/logs \
+    -o /rmt/dialogue2/interns/alejandro/logs \
+    ${SCRIPTS_PATH}run_on_grid.sh infer_eval.py \
+        --NAME eval_UNITER_basic_all_objmen_noIDs_numTargetObjs \
+        --CHECKPOINT UNITER_basic_all_objmen_noIDs_numTargetObjs \
+        --obj_id False \
+        --vis_feats_clip True \
+        --vis_feats_rcnn True \
+        --pos True \
+        --scene_seg True \
+        --obj_embs_bert True \
+        --obj_embs_sbert True \
+        --kb_id_bert False \
+        --kb_id_sbert False \
+        --obj_men True \
+        --num_target_objs_head True
+
+cd ./sh/grid_eval/
