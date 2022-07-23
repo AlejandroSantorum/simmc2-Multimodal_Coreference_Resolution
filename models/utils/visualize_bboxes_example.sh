95bdf17591979ec10484af9f99da90be17a3a147
Alejandro Santorum <alejandro.santorum@gmail.com>
2022-07-23 23:22:31 +0100

    Update notebooks

--- models/utils/visualize_bboxes_example.sh
+++ models/utils/visualize_bboxes_example.sh
@@ -2,7 +2,7 @@
 #          folders have to be unziped into 'public' folder and 'simmc2_scene_images_dstc10_public_part2' folders
 #          respectively
 
-SCENES="cloth_store_1_1_1 cloth_store_1_1_10" 
+SCENES="cloth_store_paul_19_3 cloth_store_paul_19_5 cloth_store_paul_18_2 cloth_store_paul_18_4 cloth_store_paul_18_5" 
 
 python3 visualize_bboxes.py --screenshot_root ../../data/simmc2_scene_images_dstc10_public_part2 \
                             --scene_json_root ../../data/public \
